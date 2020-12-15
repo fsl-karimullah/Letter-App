e1d59c1903e0aac0142e1284069ae1331754e1cc
fsl-karimullah <faisalbic123@gmail.com>
2020-12-16 03:04:26 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fsl-karimullah/Letter-App.git/precache-manifest.f540201dbf55b3f5f0dd0527c17ec76f.js"
+  "/fsl-karimullah/Letter-App/precache-manifest.93c3159da1cf8652a483880b2d33d69e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fsl-karimullah/Letter-App.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fsl-karimullah/Letter-App/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
